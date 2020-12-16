8396112bec68b3e93278261d65ad2c4b4c7c6503
boss-space <68944906+boss-space@users.noreply.github.com>
2020-12-16 20:58:42 +0300

    Delete Logica.js

--- Logica.js
+++
@@ -1,6 +0,0 @@
-demo=require("https").get(https://boticord.top/api/stats?servers=${guildCount}&shards=${shardCount}&users=${userCount}, {
-  headers: {
-    "Authorization": token
-  }
-});
-alert(demo) 
